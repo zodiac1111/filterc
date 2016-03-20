4774bdde98c0d6ef1124c419684efa4deee63cef
zodiac1111 <zodiac8765+github_admin@gmail.com>
2016-03-20 18:41:26 +0800

    增加一个日常预处理错误的情形的case

---
+++ test/tc1.c
@@ -0,0 +1,10 @@
+#include "tc1.h"
+void mian(void)
+{
+	a=2;
+#if A_MODEL
+	b=3
+#endif
+	
+	return 0;
+}
---
+++ test/tc1.h
@@ -0,0 +1,9 @@
+#ifndef _TC1_H
+#define _TC1_H
+
+#if A
+	int a;
+#endif
+	int b;
+
+#endif /* !deinf _TC1_H */
